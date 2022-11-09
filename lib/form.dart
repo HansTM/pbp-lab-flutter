9a5063cc00dfa014a9d52745e36f3111af21e7a1
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-11-09 21:52:02 +0700

    Lab 7: Buat halaman baru
    https://pbp-fasilkom-ui.github.io/ganjil-2023/assignments/tutorial/tutorial-7/#tutorial-membuat-halaman-baru

--- lib/form.dart
+++ lib/form.dart
@@ -1,47 +1,19 @@
-import 'package:lab/form.dart';
+import 'package:lab/main.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  final String title = 'Flutter Demo Home Page';
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyFormPage> createState() => _MyFormPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _MyFormPageState extends State<MyFormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Form'),
       ),
       // Menambahkan drawer menu
       drawer: Drawer(
@@ -75,20 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text('Hello World!'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
